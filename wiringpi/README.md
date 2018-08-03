6aad4c580fed15681f46f3412dde30f977b737ee
Simon Prickett <simon@crudworks.org>
2018-08-02 22:39:18 -0700

    Initial wiringpi example.

---
+++ wiringpi/README.md
@@ -0,0 +1,22 @@
+# TODO
+
+## Wiring Pi Method
+
+TODO Overview...
+
+### Install Wiring Pi
+
+```
+$ cd <projectsFolder>
+$ git clone git://git.drogon.net/wiringPi
+$ cd wiringPi
+$ ./build
+```
+
+### Compile and Run Code
+
+```
+$ cd wiringpi
+$ gcc -Wall -o trafficlights trafficlights.c -lwiringPi
+$ ./trafficlights
+```
---
+++ wiringpi/trafficlights.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+
+#define RED 13
+#define YELLOW 12
+#define GREEN 14
+
+void allLightsOff();
+void interruptHandler(int);
+
+void allLightsOff() {
+	digitalWrite(RED, LOW);
+	digitalWrite(YELLOW, LOW);
+	digitalWrite(GREEN, LOW);
+}
+
+void interruptHandler(int signal) {
+	allLightsOff();
+	exit(0);
+}
+
+int main(void) {
+	signal(SIGINT, interruptHandler);
+
+	if (wiringPiSetup() == -1) {
+		printf("Failed to setup Wiring Pi!\n");
+		return 1;
+	}
+
+	pinMode(RED, OUTPUT);
+	pinMode(YELLOW, OUTPUT);
+	pinMode(GREEN, OUTPUT);
+
+	allLightsOff();
+
+	while(1) {
+		// Red
+		digitalWrite(RED, HIGH);
+		delay(3000);
+
+		// Red and Yellow
+		digitalWrite(YELLOW, HIGH);
+		delay(1000);
+
+		// Green
+		digitalWrite(RED, LOW);
+		digitalWrite(YELLOW, LOW);
+		digitalWrite(GREEN, HIGH);
+		delay(5000);
+
+		// Yellow
+		digitalWrite(GREEN, LOW);
+		digitalWrite(YELLOW, HIGH);
+		delay(2000);
+
+		// Yellow off
+		digitalWrite(YELLOW, LOW);
+	}
+
+	// Never reached, keeps the compiler happy.
+	return 0;
+}
